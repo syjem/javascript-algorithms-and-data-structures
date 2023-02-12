d1b1944605797aafa4b4cb2fb94464ad5df3842e
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-12 18:09:57 +0800

    Mutation

--- index.js
+++ index.js
@@ -193,3 +193,20 @@ function getIndexToIns(arr, num) {
   }
   
   console.log(getIndexToIns([40, 60], 50));
+
+
+
+// Mutation
+
+function mutation(arr) {
+    let first = arr[0].toLowerCase();
+    let second = arr[1].toLowerCase();
+    
+    for (let char of second) {
+      if (!first.includes(char)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  console.log(mutation(["hello", "hey"]));
