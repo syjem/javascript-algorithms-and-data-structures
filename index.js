d19df53e6facdd4b9e7d1fb1bd9baaef4ec1646d
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-12 10:23:12 +0800

    Return Largest Numbers in Arrays

--- index.js
+++ index.js
@@ -39,3 +39,25 @@ console.log(factorial(5));
         return(longestWord);
     }
     console.log(wordLength("The quick brown fox jumped over the lazy dog"));
+
+
+
+    // Return Largest Numbers in Arrays
+
+    function largestOfFour(arr) {
+        const results = [];
+         for (let i = 0; i < arr.length; i++) {
+           let largestNumber = arr[i][0];
+           for (let j = 1; j < arr[i].length; j++) {
+             if (arr[i][j] > largestNumber) {
+               largestNumber = arr[i][j];
+             }
+           }
+           results[i] = largestNumber;
+         }
+       
+         return results;
+       }
+
+       console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+       console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
