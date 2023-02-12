dc99b0bbafd1f24e647d675fc87b2b2bc26436e7
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-12 09:26:53 +0800

    Factorial of a number

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ function reversedString(str) {
 }
 
 console.log(reversedString("Hello World"));
+
+
+// Factorial of a number
+
+function factorial(n) {
+    if(n < 0) return undefined;
+    if(n===0) return 1;
+    
+    let result = 1;
+    for(let i =1; i<=n; i++) {
+        result *= i;
+    }
+    return(result);
+}
+console.log(factorial(5));
