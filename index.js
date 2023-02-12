6aa0b844fecec9d0c3c79f3e338416264e8e49b9
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-12 18:14:44 +0800

    Chunk an Array in Groups

--- index.js
+++ index.js
@@ -210,3 +210,18 @@ function mutation(arr) {
     return true;
   }
   console.log(mutation(["hello", "hey"]));
+
+
+
+// Write a function that splits an array (first argument) 
+// into groups the length of size (second argument) 
+// and returns them as a two-dimensional array.  
+
+function chunkArrayInGroups(arr, size) {
+    let result = [];
+    for (let i = 0; i < arr.length; i += size) {
+      result.push(arr.slice(i, i + size));
+    }
+    return result;
+  }
+  console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
