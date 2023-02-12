4aad1bc37b011e6798a8650a989f1dacd59d748f
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-12 18:06:00 +0800

    Where do I belong?

--- index.js
+++ index.js
@@ -170,3 +170,26 @@ console.log(factorial(5));
     });
     } 
   console.log(bouncer([7, "ate", "", false, 9]));
+
+
+
+// Where do I belong?
+//  Return the lowest index at which a value (second argument) 
+// should be inserted into an array (first argument) once it has 
+// been sorted. The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+    arr.sort(function(a, b) {
+      return a - b;
+    });
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= num) {
+        return i;
+      }
+    }
+  
+    return arr.length;
+  }
+  
+  console.log(getIndexToIns([40, 60], 50));
