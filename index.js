fc4098b1da3282ff36d63ad6f2a8c1fb7a02b5e8
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-12 09:53:01 +0800

    Find the Longest Word in a String

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ console.log(reversedString("Hello World"));
 function factorial(n) {
     if(n < 0) return undefined;
     if(n===0) return 1;
-    
+
     let result = 1;
     for(let i =1; i<=n; i++) {
         result *= i;
@@ -23,3 +23,19 @@ function factorial(n) {
     return(result);
 }
 console.log(factorial(5));
+
+
+
+// Find the Longest Word in a String
+
+    function wordLength(str) {
+        let words = str.split(" ");
+        let longestWord = 0;
+
+        for(let word of words) {
+            longestWord = Math.max(longestWord, word.length);
+        }
+
+        return(longestWord);
+    }
+    console.log(wordLength("The quick brown fox jumped over the lazy dog"));
