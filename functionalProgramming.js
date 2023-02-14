bfe0c1f2ed80d63eb23e317988516362dc75464a
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-14 14:24:38 +0800

    some method

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -12,3 +12,14 @@ function checkPositive(arr) {
     return arr.every(val => val > 0);
   }
   console.log(checkPositive([1, 2, 3, -4, 5]));
+
+
+
+
+// Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+function checkPositive(arr) {
+  
+    return arr.some(val => val > 0);
+  }
+  console.log(checkPositive([1, 2, 3, -4, 5]));
