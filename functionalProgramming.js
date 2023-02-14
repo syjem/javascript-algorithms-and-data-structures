3cb2effe73b233f91d1372aac181bffea82f9f66
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-14 14:19:44 +0800

    every method

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -4,3 +4,11 @@ function urlSlug(title) {
     return title.toLowerCase().trim().split(/\s+/).join("-");
   }
   console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+
+
+// Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+function checkPositive(arr) {
+    return arr.every(val => val > 0);
+  }
+  console.log(checkPositive([1, 2, 3, -4, 5]));
