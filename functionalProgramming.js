7ddbaa2e0298f5609a2941cdeebeb4c12f26454f
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-14 14:19:14 +0800

    convert strings to URL

---
+++ functionalProgramming.js
@@ -0,0 +1,6 @@
+// Apply Functional Programming to Convert Strings to URL Slugs
+
+function urlSlug(title) {
+    return title.toLowerCase().trim().split(/\s+/).join("-");
+  }
+  console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
