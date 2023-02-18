23a725464cae620919c0f2d8b465d1ffcb5857a6
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-18 14:40:55 +0800

    Seek and Destroy

--- js/intermediateAlgorithmScripting.js
+++ js/intermediateAlgorithmScripting.js
@@ -29,3 +29,16 @@ function diffArray(arr1, arr2) {
   }
   
   console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+
+
+// Seek and Destroy
+
+function destroyer(arr) {
+    // get the arguments object as an array, starting at index 1
+    const args = Array.from(arguments).slice(1);
+  
+    // filter out all elements in the array that are present in the arguments
+    return arr.filter(x => !args.includes(x));
+  }
+  console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
