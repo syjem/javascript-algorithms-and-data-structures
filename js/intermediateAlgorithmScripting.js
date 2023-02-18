80c0350de94b3d70e99d3e4faef5d21875ec5953
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-18 14:32:34 +0800

    Sum All Numbers in a Range

---
+++ js/intermediateAlgorithmScripting.js
@@ -0,0 +1,18 @@
+// Intermediate Algorithm Scripting
+
+
+
+// Sum All Numbers in a Range
+
+function sumAll(arr) {
+
+    arr.sort((a, b) => a - b);
+
+    let sum = 0;
+    for(let i = arr[0]; i<=arr[1]; i++) {
+        sum += i;
+
+    }
+    return(sum);
+}
+console.log(sumAll([1, 4]));
