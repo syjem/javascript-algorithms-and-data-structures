d37689bd5d0307247378bbeabea24e2157d6e1f4
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-18 14:37:29 +0800

    Diff Two Arrays

--- js/intermediateAlgorithmScripting.js
+++ js/intermediateAlgorithmScripting.js
@@ -16,3 +16,16 @@ function sumAll(arr) {
     return(sum);
 }
 console.log(sumAll([1, 4]));
+
+
+
+// Diff Two Arrays
+
+function diffArray(arr1, arr2) {
+  
+    const diff1 = arr1.filter(x => !arr2.includes(x));
+    const diff2 = arr2.filter(x => !arr1.includes(x));
+    return diff1.concat(diff2);
+  }
+  
+  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
