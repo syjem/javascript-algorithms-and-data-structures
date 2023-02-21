695e78673adb6e56077eb5ec2da50eee2fea7f3f
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-21 19:57:57 +0800

    Everything Be True

--- js/intermediateAlgorithmScripting.js
+++ js/intermediateAlgorithmScripting.js
@@ -211,3 +211,13 @@ function binaryAgent(str) {
 }
 
 console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+
+
+// Everything Be True - Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+function truthCheck(collection, pre) {
+  return collection.every(obj => obj[pre]);
+}
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
