b95635d1c63212b58fb9060c1fb8b9809c714ecc
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-21 20:06:29 +0800

    Arguments Optional

--- js/intermediateAlgorithmScripting.js
+++ js/intermediateAlgorithmScripting.js
@@ -221,3 +221,24 @@ function truthCheck(collection, pre) {
 }
 
 console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+
+
+
+// Arguments Optional - Create a function that sums two arguments together. 
+// If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+function addTogether() {
+  
+  const [first, second] = arguments;
+  if (typeof(first) !== "number")
+    return undefined;
+  if (arguments.length === 1)
+    return (second) => addTogether(first, second);
+  if (typeof(second) !== "number")
+    return undefined;
+  return first + second;
+}
+
+console.log(addTogether(2,3));
+console.log(addTogether(2, "3"));
+console.log(addTogether(2)([3]));
