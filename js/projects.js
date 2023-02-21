f047be77f7122b6e8661701afd1082bccadce456
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-21 20:44:15 +0800

    cashindrawer

--- js/projects.js
+++ js/projects.js
@@ -76,3 +76,63 @@ function telephoneCheck(str) {
  }
  
  console.log(telephoneCheck("555-555-5555"));
+
+
+
+//  Cash Register
+
+function checkCashRegister(price, cash, cid) {
+    const currencyValues = {
+      "PENNY": 0.01,
+      "NICKEL": 0.05,
+      "DIME": 0.1,
+      "QUARTER": 0.25,
+      "ONE": 1,
+      "FIVE": 5,
+      "TEN": 10,
+      "TWENTY": 20,
+      "ONE HUNDRED": 100
+    };
+    
+    let changeDue = cash - price;
+    let cidTotal = 0;
+    let change = [];
+    
+    for (let i = 0; i < cid.length; i++) {
+      cidTotal += cid[i][1];
+    }
+    
+    if (cidTotal < changeDue) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+    
+    if (cidTotal === changeDue) {
+      return {status: "CLOSED", change: cid};
+    }
+    
+    for (let i = cid.length - 1; i >= 0; i--) {
+      let currencyName = cid[i][0];
+      let currencyValue = currencyValues[currencyName];
+      let currencyAvailable = cid[i][1];
+      let currencyUsed = 0;
+      let currencyCount = 0;
+      while (changeDue >= currencyValue && currencyAvailable > 0) {
+        changeDue -= currencyValue;
+        changeDue = Math.round(changeDue * 100) / 100;
+        currencyAvailable -= currencyValue;
+        currencyUsed += currencyValue;
+        currencyCount++;
+      }
+      if (currencyCount > 0) {
+        change.push([currencyName, currencyUsed]);
+      }
+    }
+    
+    if (changeDue > 0) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+    
+    return {status: "OPEN", change: change};
+  }
+  
+  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
