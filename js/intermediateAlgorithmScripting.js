6c1f094525324333638930bcfe16967effe2ef8e
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-21 19:38:53 +0800

    Sum all Primes

--- js/intermediateAlgorithmScripting.js
+++ js/intermediateAlgorithmScripting.js
@@ -155,3 +155,19 @@ function fearNotLetter(str) {
 }
 
 console.log(fearNotLetter("abce"));
+
+
+
+// Sum all Primes
+
+function sumPrimes(num) {
+  // Check all numbers for primality
+  let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0))
+      primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+}
+
+console.log(sumPrimes(10));
