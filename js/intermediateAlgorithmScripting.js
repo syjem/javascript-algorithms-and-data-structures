cb192a3df4c19bc707a5cdff7ce538ff40e03a4a
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-21 19:46:52 +0800

    Drop it

--- js/intermediateAlgorithmScripting.js
+++ js/intermediateAlgorithmScripting.js
@@ -171,3 +171,18 @@ function sumPrimes(num) {
 }
 
 console.log(sumPrimes(10));
+
+
+
+// Drop it
+
+function dropElements(arr, func) {
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+}
+
+console.log(dropElements([1, 2, 3, 4], function(n) {
+  return n >= 3;
+}));
